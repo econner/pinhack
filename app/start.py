c7b430e7e82b14da5d90f96a001a1615f5bf9723
Marshall <mmoutenot@gmail.com>
2013-01-11 15:07:45 -0800

    modify root url to generate new board and redirect. add models for item and board

--- app/start.py
+++ app/start.py
@@ -1,20 +1,25 @@
+import uuid
 import tornado.ioloop
 import tornado.web
 import options
 import redis
-
+from models import Item, Board
 
 class MainHandler(tornado.web.RequestHandler):
-    def get(self):
-        r = redis.StrictRedis(host='localhost', port=6379, db=0)
-        r.set('foo', 'bar')
-        self.write("Hello world!  Redis said: %s" % r.get('foo'))
+  def get(self):
+    b = Board()
+    b.id = uuid.uuid4()
+    b.read_only = uuid.uuid4()
+    print 'created a board'
+    self.redirect('/'+str(b.id))
 
 application = tornado.web.Application([
-    (r"/", MainHandler),
-    (r'/static/(.*)', tornado.web.StaticFileHandler, {'path': 'static/'}),
+  (r"/", MainHandler),
+  (r'/static/(.*)', tornado.web.StaticFileHandler, {'path': 'static/'}),
 ])
 
 if __name__ == "__main__":
-    application.listen(options.cli_args.port)
-    tornado.ioloop.IOLoop.instance().start()
+  application.listen(options.cli_args.port)
+  tornado.ioloop.IOLoop.instance().start()
+  r = redis.StrictRedis(host='localhost', port=6379, db=0)
+  r.set('foo', 'bar')
