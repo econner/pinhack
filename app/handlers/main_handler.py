4ebef895ca313305686f2edb066fd3e0c1546f9d
Marshall <mmoutenot@gmail.com>
2013-01-11 18:09:01 -0800

    remove redundant save

--- app/handlers/main_handler.py
+++ app/handlers/main_handler.py
@@ -9,13 +9,12 @@ class MainHandler(tornado.web.RequestHandler):
   def get(self):
     b = Board(id=uuid.uuid4(), read_only=uuid.uuid4())
     b.save()
-    r.set("b_" + str(b.id), str(b.to_json()))
     print 'created a board'
     self.redirect('/'+str(b.id))
 
 class BoardHandler(tornado.web.RequestHandler):
   def get(self, board_id):
-    board = Board.get_from_db(board_id)
+    board = Board.get(board_id)
     if not board:
       raise tornado.web.HTTPError(404)
     self.render('../../static/index.html', board=board)
