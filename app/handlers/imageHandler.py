3b745fc13fcf54b30b466e88948354e0d1701d83
Yixue Li <liyixue.ray@gmail.com>
2013-01-12 13:33:23 -0500

    add image url

--- app/handlers/imageHandler.py
+++ app/handlers/imageHandler.py
@@ -5,8 +5,5 @@ import json
 
 class ImageHandler(tornado.web.RequestHandler):
 	def get(self):
-		link = self.get_argument('url')
-		if endings(link) in ('.jpg','.jpeg','.gif'):
-			print link
-		#mageURLs = imageGetter.getImages(self.get_argument('url'))
-		#elf.write(json.dumps(imageURLs))
+		imageURLs = imageGetter.getImages(self.get_argument('url'))
+		self.write(json.dumps(imageURLs))
