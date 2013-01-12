8e88e4af699db1ac6987d24e3c31d71329286f0d
Kidai Kwon <kidai@kidai-Studio-1558.(none)>
2013-01-11 22:39:49 -0500

    adding reveal

--- app/start.py
+++ app/start.py
@@ -3,9 +3,11 @@ import tornado.web
 import options
 
 from handlers import main_handler
+from handlers import imageHandler
 
 application = tornado.web.Application([
   (r"/", main_handler.MainHandler),
+  (r"/image/", imageHandler.ImageHandler),
   (r'/static/(.*)', tornado.web.StaticFileHandler, {'path': 'static/'}),
 ])
 
