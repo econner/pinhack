c925fa6352cb13d0f6bd8e8d66ff3c68ed44862e
Yixue Li <liyixue.ray@gmail.com>
2013-01-12 00:03:27 -0500

    add user_id to listen_handler.py

--- test_api.sh
+++ test_api.sh
@@ -1,4 +1,4 @@
 curl -i \
     -H "Accept: application/json" \
-    -X PUT -d "board_id=afa978c1-74cc-4748-b4a5-48ec13841c29&url=http://www.google.com&image_url=http://www.fellowearthlings.org/images/home_meerkat.jpg&tags=[abc,def]&pos_x=40.5&pos_y=30.2&scale=1.3&locked=true"\
+    -X PUT -d "board_id=1afa3275-1ee7-4f5b-8417-4ac2b8f0f294&url=http://www.google.com&image_url=http://www.fellowearthlings.org/images/home_meerkat.jpg&tags=[abc,def]&pos_x=40.5&pos_y=30.2&scale=1.3&locked=true"\
     http://localhost:7100/add_item/
