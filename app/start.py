761c07836e215cff58c3d19fc7658da596c8db40
Yixue Li <liyixue.ray@gmail.com>
2013-01-11 19:50:32 -0500

    add listen_handler.py and modified start.py

--- app/start.py
+++ app/start.py
@@ -1,14 +1,17 @@
 import tornado.ioloop
+import tornado.httpserver
 import tornado.web
 import options
-
+from handlers import listen_handler
 from handlers import main_handler
 
 application = tornado.web.Application([
+  (r'/ws', listen_handler.EchoWebSocket),
   (r"/", main_handler.MainHandler),
   (r'/static/(.*)', tornado.web.StaticFileHandler, {'path': 'static/'}),
 ])
 
 if __name__ == "__main__":
+  http_server = tornado.httpserver.HTTPServer(application)
   application.listen(options.cli_args.port)
   tornado.ioloop.IOLoop.instance().start()
