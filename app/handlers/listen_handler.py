5c01a68e36f672cd5dd36b13923079349596d894
Evrhet Milam <evrhet@pinterest.com>
2013-01-11 17:40:56 -0800

    Add import.

--- app/handlers/listen_handler.py
+++ app/handlers/listen_handler.py
@@ -1,9 +1,11 @@
 import json
 from tornado import websocket
 
+from models import Board
+
 class EchoWebSocket(websocket.WebSocketHandler):
   def open(self, board_id):
-    board = Board.get_from_db(board_id)
+    board = Board.get(board_id)
     self.write_message(json.dumps({'board': board.to_json()}))
 
   def on_message(self, message):
