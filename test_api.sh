9fc020ff19e3c4f6a2e5cb21baf1efb2c3a43e17
dfinzer <devin_finzer@brown.edu>
2013-01-11 18:31:33 -0800

    Pipeline working

--- test_api.sh
+++ test_api.sh
@@ -1,4 +1,4 @@
 curl -i \
     -H "Accept: application/json" \
-    -X PUT -d "board_id=211e39d9-92da-4944-a6ad-bd6c5e7f6cd8&url=http://www.google.com&image_url=http://www.amazon.com&tags=[abc,def]&pos_x=40.5&pos_y=30.2&scale=1.3&locked=true"\
+    -X PUT -d "board_id=0224107f-e2b8-4d85-8f9d-0eb6a515b380&url=http://www.google.com&image_url=http://www.fellowearthlings.org/images/home_meerkat.jpg&tags=[abc,def]&pos_x=40.5&pos_y=30.2&scale=1.3&locked=true"\
     http://localhost:7100/add_item/
