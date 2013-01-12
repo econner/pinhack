cb6c9ad5857ca4dd02dad4c574b08d00f65c829f
Eric Conner <eric@pinterest.com>
2013-01-12 13:35:56 -0800

    Persist username in cookie.

--- static/js/user_display.js
+++ static/js/user_display.js
@@ -3,14 +3,31 @@
  */
 var UserDisplay = (function() {
 
-	var saveCurrentUser = function() {
-		var cur_user = $('.current-user');
+	var cur_username = null;
+
+	var saveCurrentUser = function(username) {
+		if(!username) {
+			username = $('.current-user').text();
+		}
 		var data = {
 			'message_type': 'user_update',
-			'username': cur_user.text(),
+			'username': username,
 			'board_id': boardId
 		};
+		console.info(data);
+		var old_username = $.cookie("username");
+		console.info("OLD USERNAME:" + old_username);
+		console.info("NEW USERNAME:" + username);
 		socket.send(JSON.stringify(data));
+		$.cookie("username", username);
+	};
+
+	var checkForUsername = function() {
+		console.info("USERNAME:" + $.cookie("username"));
+		if($.cookie("username") !== "") {
+			cur_username = $.cookie("username");
+			console.info("USERNAME IS:" + cur_username);
+		}
 	};
 
 	var renderUserDisplay = function(users_connected, current_user) {
@@ -19,9 +36,15 @@ var UserDisplay = (function() {
 		$.each(users_connected, function(idx, val) {
 			var cur_user = $('<li>' + val + '</li>');
 			if(val == current_user) {
+				if(cur_username && cur_username !== current_user) {
+					cur_user.text(cur_username);
+					saveCurrentUser(cur_username);
+				}
 				cur_user.addClass('current-user');
 				cur_user.attr("contenteditable", "true");
-				cur_user.bind("blur", saveCurrentUser);
+				cur_user.bind("blur", function(ev) {
+					saveCurrentUser();
+				});
 			}
 			user_display.append(cur_user);
 		});
@@ -30,11 +53,11 @@ var UserDisplay = (function() {
 
 	var render = function(data) {
 		renderUserDisplay(data['users_connected'], data['current_user']);
-
 	};
 
 	/* Public interface */
 	return {
-		render: render
+		render: render,
+		checkForUsername: checkForUsername
 	};
 })();
